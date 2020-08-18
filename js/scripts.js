9239aadd40ab3157fc57764b7e341efc082e21d8
Vanessa Stewart <vstewart1213@gmail.com>
2020-08-18 13:57:59 -0700

    Add bootstrap styling and refactor code

--- js/scripts.js
+++ js/scripts.js
@@ -14,32 +14,10 @@ $(document).ready(function () {
         newArray[i] = stringInputArray[i];
       };
     };
-    console.log(newArray);
     let newString = newArray.join("");
     $("#return").text(newString);
     $("form").hide();
     $("#returnDiv").show();
   });
 });
-
-// $(document).ready(function () {
-//   $("form").submit(function(event){
-//     event.preventDefault();
-//     const stringInput = $("#string").val();
-//     console.log(stringInput);
-//     let newString = "";
-//     console.log(newString);
-//     for (let i=0; i<stringInput.length; i+=1) {
-//       if (stringInput[i] === "a" || stringInput[i] === "A" || stringInput[i] === "e" || stringInput[i] === "E" || stringInput[i] === "i" || stringInput[i] === "I" || stringInput[i] === "o" || stringInput[i] === "O" || stringInput[i] === "u" || stringInput[i] === "U" ) {
-//         newString[i] = "-";
-//       } else {
-//         newString[i] = stringInput[i];
-//       };
-//     };
-//     console.log(newString);
-//     $("#return").text(newString);
-//     $("form").hide();
-//     $("#returnDiv").show();
-//   });
-// });
 
